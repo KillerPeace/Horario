ad403863a6e3b419d576f3e9ea4ebe454e0e4848
killerpeace~ <contaffpc10@gmail.com>
2023-06-01 22:51:32 -0300

    Criando novo sistema : Grana

---
+++ grana.js
@@ -0,0 +1,13 @@
+/*
+COndição : 
+Se caso for [true (veradeiro), eu vou sair de casa se tiver dinheiro]
+Se caso for [false (falso), eu nao vou sair de casa, porque não tenho dinheiro]
+*/
+
+const tenhoGrana = true;
+
+if(tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Nao vou sair de casa');
+}
