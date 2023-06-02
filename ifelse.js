dc9aae6e21e58cfa4cd030efe5b38af74de742bd
killerpeace~ <contaffpc10@gmail.com>
2023-06-01 23:30:58 -0300

    Criando novo sistema : ifelse

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+/*
+Se o numero estiver entre 0 - 5, será executado no console log a condiçao.
+*/
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O numero está entre 0 - 5.');
+} else {
+    console.log('O numero não está entre 0 - 5.');
+}
