a380d23dd9b175289b7fa6c6d6b125a40ea4c942
killerpeace~ <contaffpc10@gmail.com>
2023-06-01 22:44:10 -0300

    Alteração de algumas linhas

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ Se a hora for maior que 17 e menor que 23, nosso sistema dará boa noite
 
 */
 
-const hora = 10;
+const hora = 50;
 
 if (hora >= 0 && hora <= 12)
 {
     console.log('bom dia');
-}
-
-else if (hora >= 12 && hora <= 17) {
+} else if (hora >= 12 && hora <= 17) {
 
     console.log('boa tarde');
-}
-
-else if (hora >= 17 && hora <= 23) {
+} else if (hora >= 17 && hora <= 23) {
 
     console.log('boa tarde');
 }
