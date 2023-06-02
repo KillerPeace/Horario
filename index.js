da36b2eab9abd06c3a1031ccae3cc6fa8b314d90
killerpeace~ <contaffpc10@gmail.com>
2023-06-01 22:42:05 -0300

    Finalizacao do sistema

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+/*
+Sistema de horas em java script
+Se a hora for maior que 0 e menor que 12, nosso sistema dará bom dia
+Se a hora for maior que 12 e menor que 17, nosso sistema dará boa tarde
+Se a hora for maior que 17 e menor que 23, nosso sistema dará boa noite
+
+*/
+
 const hora = 10;
 
-if (hora <= 12)
+if (hora >= 0 && hora <= 12)
 {
-    console.log('bom dia tudo bem?');
+    console.log('bom dia');
 }
+
+else if (hora >= 12 && hora <= 17) {
+
+    console.log('boa tarde');
+}
+
+else if (hora >= 17 && hora <= 23) {
+
+    console.log('boa tarde');
+}
+
+
+
